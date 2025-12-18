89be5ce6683b3f3c3edb942fbf8ddcf3770fb287
at1047 <andrewhytai@gmail.com>
2025-12-17 21:02:58 -0800

    change to use omega, add description

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Plugin, PluginSettingTab, Setting } = require('obsidian');
+const { Plugin, PluginSettingTab, Setting, Notice } = require('obsidian');
 
 module.exports = class HideSortingNumbersPlugin extends Plugin {
     async onload() {
@@ -10,12 +10,19 @@ module.exports = class HideSortingNumbersPlugin extends Plugin {
                 this.containerEl.empty();
                 new Setting(this.containerEl)
                     .setName('Hide sorting numbers')
-                    .setDesc('Hide "_number " and "z_number " prefixes')
+                    .setDesc('Hide "_number " and "Ω_number " prefixes')
                     .addToggle(t => t.setValue(this.plugin.settings.hideNumbers).onChange(async v => {
                         this.plugin.settings.hideNumbers = v;
                         await this.plugin.saveData(this.plugin.settings);
                         this.plugin.updateState();
                     }));
+                
+                const infoDiv = this.containerEl.createDiv({ cls: 'setting-item-description' });
+                infoDiv.appendText('To type Ω: Mac: Option+Z | Windows: Alt+234 | Or copy: ');
+                const copyInput = infoDiv.createEl('input', { type: 'text', value: 'Ω' });
+                copyInput.style.width = '30px';
+                copyInput.style.textAlign = 'center';
+                copyInput.readOnly = true;
             }
         }(this.app, this));
 
@@ -24,13 +31,29 @@ module.exports = class HideSortingNumbersPlugin extends Plugin {
         this.observer = new MutationObserver(() => this.settings.hideNumbers && this.processAll());
         this.observer.observe(document.body, { childList: true, subtree: true });
 
+        // Update when switching tabs/panes
+        this.registerEvent(this.app.workspace.on('active-leaf-change', () => {
+            this.processAll();
+        }));
+
         this.addCommand({
             id: 'toggle-hide-sorting-numbers',
             name: 'Toggle hide sorting numbers',
             callback: async () => {
+                console.log('Toggle command triggered');
                 this.settings.hideNumbers = !this.settings.hideNumbers;
                 await this.saveData(this.settings);
                 this.updateState();
+                new Notice(`Hide sorting numbers: ${this.settings.hideNumbers ? 'ON' : 'OFF'}`, 5000);
+                console.log('Notice should have appeared');
+            }
+        });
+
+        this.addCommand({
+            id: 'debug-notice',
+            name: 'Debug: Show test notice',
+            callback: () => {
+                new Notice('Debug notice works!');
             }
         });
     }
@@ -42,10 +65,14 @@ module.exports = class HideSortingNumbersPlugin extends Plugin {
 
     processAll() {
         document.querySelectorAll('.nav-file-title-content, .nav-folder-title-content, .inline-title, .view-header-title, .titlebar-text, .workspace-tab-header-inner-title').forEach(el => {
-            const match = el.textContent.match(/^(_\d+[\.\-\s]+|z_\d+[\.\-\s]+)/i);
+            const match = el.textContent.match(/^(_\d+[\.\-\s]+|Ω_\d+[\.\-\s]+)/i);
             if (match) {
                 el.style.setProperty('--original-font-size', el.style.getPropertyValue('--original-font-size') || getComputedStyle(el).fontSize);
                 el.setAttribute('data-display-text', el.textContent.substring(match[1].length));
+            } else if (el.hasAttribute('data-display-text')) {
+                // Clear attribute if element no longer has a sorting prefix
+                el.removeAttribute('data-display-text');
+                el.style.removeProperty('--original-font-size');
             }
         });
     }
